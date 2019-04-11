e1d9a2e74485d8da93f8b4b115b89a391a1fea32
HANA LEANDER <hana0070@gmail.com>
2019-04-11 02:59:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,16 @@
 def reverse_each_word(sentence)
-  sentence.collect do |sentence|
+  sentence.reverse.split do |sentence|
     sentence.reverse.to_s
   end
 end
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
